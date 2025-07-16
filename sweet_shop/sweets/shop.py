792f96e45c527320704228a1421f2f2ca2d71df7
Rishabh <prajapatirishabh04@gmail.com>
2025-07-16 20:00:00 +0530

    Feature: implement delete_sweet method to allow removal by sweet ID

--- sweet_shop/sweets/shop.py
+++ sweet_shop/sweets/shop.py
@@ -7,3 +7,7 @@ class SweetShop:
 
     def get_all_sweets(self):
         return self._inventory
+
+    def delete_sweet(self, sweet_id: int):
+        self._inventory = [s for s in self._inventory if s.id != sweet_id]
+    
